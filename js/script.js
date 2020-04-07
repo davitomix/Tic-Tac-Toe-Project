4825d573d52ee32cc8a5bda80755e2eaa954b2b5
David Elì <davidelimg1@gmail.com>
2020-04-07 15:58:39 -0500

    game functions encapsulated

--- js/script.js
+++ js/script.js
@@ -17,13 +17,13 @@ const winningMsgTxtElement = document.querySelector('[data-winning-msg-txt]');
 const resetBtn = document.getElementById('resetBtn');
 let circleTurn = false;
 
-const isDraw = () =>{
+const isDraw = () => {
     return [...cellElements].every(cell => {
       return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
     })
-  }
+}
 
-const endGame = (draw) =>{
+const endGame = (draw) => {
     if(draw) {
       winningMsgTxtElement.innerText = 'Draw!';
     } else {
@@ -34,9 +34,9 @@ const endGame = (draw) =>{
 
 const placeMark = (cell, currentClass) => {
     cell.classList.add(currentClass);
-  }
+}
 
-const swapTurns = () =>{
+const swapTurns = () => {
   circleTurn = !circleTurn;
 }
 
@@ -57,39 +57,38 @@ const checkWin = (currentClass) => {
         return cellElements[index].classList.contains(currentClass);
       })
     })
-  }
+}
 
-const handleClick = (e) =>{
-    
-    if (e)
-    {
-        const cell = e.target;
-        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-        placeMark(cell, currentClass);
-        if(checkWin(currentClass)) {
-        endGame(false);
-        } else if(isDraw()) {
-        endGame(true);
-        } else {
-        swapTurns();
-        setBoardHoverClass();
-        }
+const handleClick = (e) => {
+    if (e) {
+      const cell = e.target;
+      const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+      placeMark(cell, currentClass);
+      if(checkWin(currentClass)) {
+      endGame(false);
+      } else if(isDraw()) {
+      endGame(true);
+      } else {
+      swapTurns();
+      setBoardHoverClass();
+      }
     }
 }
 
-const startGame = (e) =>{
-    cellElements.forEach(cell => {
-      cell.classList.remove(X_CLASS);
-      cell.classList.remove(CIRCLE_CLASS);
-      cell.removeEventListener('click', handleClick);
-      cell.addEventListener('click', handleClick, { once: true})
-    })
-    setBoardHoverClass();
-    winningMsgElement.classList.remove('show');
-}
+const game = (() => {
+  const startGame = (e) => {
+      cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true})
+      })
+      setBoardHoverClass();
+      winningMsgElement.classList.remove('show');
+  };
+  resetBtn.addEventListener('click', startGame);
+  startGame();
+})();
 
-resetBtn.addEventListener('click', startGame);
-startGame();
-handleClick();
 
 
