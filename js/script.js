8cbf2fd679001b8e3039c5b015902260cb1c70d0
David Elì <davidelimg1@gmail.com>
2020-04-07 16:52:11 -0500

    Added boxes for players

--- js/script.js
+++ js/script.js
@@ -18,22 +18,22 @@ const resetBtn = document.getElementById('resetBtn');
 let circleTurn = false;
 
 const isDraw = () => {
-    return [...cellElements].every(cell => {
-      return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
-    })
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+  })
 }
 
 const endGame = (draw) => {
-    if(draw) {
-      winningMsgTxtElement.innerText = 'Draw!';
-    } else {
-      winningMsgTxtElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
-    }
-    winningMsgElement.classList.add('show');
+  if(draw) {
+    winningMsgTxtElement.innerText = 'Draw!';
+  } else {
+    winningMsgTxtElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMsgElement.classList.add('show');
 }
 
 const placeMark = (cell, currentClass) => {
-    cell.classList.add(currentClass);
+  cell.classList.add(currentClass);
 }
 
 const swapTurns = () => {
@@ -41,54 +41,69 @@ const swapTurns = () => {
 }
 
 const setBoardHoverClass = () => {
-    board.classList.remove(X_CLASS);
-    board.classList.remove(CIRCLE_CLASS);
-    if(circleTurn) {
-      board.classList.add(CIRCLE_CLASS);
-    }
-    else {
-      board.classList.add(X_CLASS);
-    }
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  if(circleTurn) {
+    board.classList.add(CIRCLE_CLASS);
+  }
+  else {
+    board.classList.add(X_CLASS);
+  }
 }
 
 const checkWin = (currentClass) => {
-    return WINNING_COMBINATIONS.some(combination => {
-      return combination.every(index => {
-        return cellElements[index].classList.contains(currentClass);
-      })
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
     })
+  })
 }
 
 const handleClick = (e) => {
-    if (e) {
-      const cell = e.target;
-      const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-      placeMark(cell, currentClass);
-      if(checkWin(currentClass)) {
-      endGame(false);
-      } else if(isDraw()) {
-      endGame(true);
-      } else {
-      swapTurns();
-      setBoardHoverClass();
-      }
+  if (e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if(checkWin(currentClass)) {
+    endGame(false);
+    } else if(isDraw()) {
+    endGame(true);
+    } else {
+    swapTurns();
+    setBoardHoverClass();
     }
+  }
 }
 
 const game = (() => {
   const startGame = (e) => {
-      cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS);
-        cell.classList.remove(CIRCLE_CLASS);
-        cell.removeEventListener('click', handleClick);
-        cell.addEventListener('click', handleClick, { once: true})
-      })
-      setBoardHoverClass();
-      winningMsgElement.classList.remove('show');
+    cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true})
+  })
+    setBoardHoverClass();
+    winningMsgElement.classList.remove('show');
   };
   resetBtn.addEventListener('click', startGame);
   startGame();
-})();
+});
 
-
+window.onload = function ready() {
+  const playerForm = document.getElementById('player-form');
+  playerForm.reset();
+  const btn = document.getElementById('submit');
+  const select = document.querySelectorAll('.player-input');
+  select.forEach(item => {
+    const ipt = item.getElementsByTagName('input');
+    ipt[0].addEventListener('input', () => {
+      if (playerForm.name1.value > '' && playerForm.name2.value > '') {
+        btn.disabled = false;
+      } else {
+        btn.disabled = true;
+      }
+    });
+  });
+};
 
