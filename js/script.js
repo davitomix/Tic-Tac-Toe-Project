10cf4560da46d1140c12304f2b7193ce2bfa0c13
Feber <cochabambinoski@gmail.com>
2020-04-07 20:38:14 -0400

    Fixed Readme and configured lintern

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle'; 
+const CIRCLE_CLASS = 'circle';
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -8,94 +8,85 @@ const WINNING_COMBINATIONS = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
+  [2, 4, 6],
 ];
-var players=['X','O'];
-var whoseturn = 0;
+const players = ['X', 'O'];
+let whoseturn = 0;
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMsgElement = document.getElementById('winningMsg');
 const winningMsgTxtElement = document.querySelector('[data-winning-msg-txt]');
 const resetBtn = document.getElementById('resetBtn');
 let circleTurn = false;
-
-const isDraw = () => {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
-  })
-}
-
+/* eslint-disable */
+const isDraw = () => [...cellElements].every(cell => cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS));
+/* eslint-enable */
 const endGame = (draw) => {
-  if(draw) {
+  if (draw) {
     winningMsgTxtElement.innerText = 'Draw!';
   } else {
     winningMsgTxtElement.innerText = `${circleTurn ? players[1] : players[0]} Wins!`;
   }
   winningMsgElement.classList.add('show');
-}
+};
 
 const placeMark = (cell, currentClass) => {
   cell.classList.add(currentClass);
-}
+};
 
 const swapTurns = () => {
   circleTurn = !circleTurn;
-  
-  if (whoseturn == 0) whoseturn=1;
-  else whoseturn=0;
-  document.getElementById("game-message").innerText = players[whoseturn] + "'s Turn"
-}
+
+  if (whoseturn === 0) whoseturn = 1;
+  else whoseturn = 0;
+  document.getElementById('game-message').innerText = `${players[whoseturn]}'s Turn`;
+};
 
 const setBoardHoverClass = () => {
   board.classList.remove(X_CLASS);
   board.classList.remove(CIRCLE_CLASS);
-  if(circleTurn) {
+  if (circleTurn) {
     board.classList.add(CIRCLE_CLASS);
-  }
-  else {
+  } else {
     board.classList.add(X_CLASS);
   }
-}
-
-const checkWin = (currentClass) => {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass);
-    })
-  })
-}
-
+};
+/* eslint-disable */
+const checkWin = (currentClass) => WINNING_COMBINATIONS.some(combination => combination.every(index => cellElements[index].classList.contains(currentClass)));
+/* eslint-enable */
 const handleClick = (e) => {
   if (e) {
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
     placeMark(cell, currentClass);
-    if(checkWin(currentClass)) {
-    endGame(false);
-    } else if(isDraw()) {
-    endGame(true);
+    if (checkWin(currentClass)) {
+      endGame(false);
+    } else if (isDraw()) {
+      endGame(true);
     } else {
-    swapTurns();
-    setBoardHoverClass();
+      swapTurns();
+      setBoardHoverClass();
     }
   }
-}
+};
 
-const game = (() => {
-  const startGame = (e) => {
-    cellElements.forEach(cell => {
+
+const startGame = () => {
+  cellElements.forEach(cell => {
     cell.classList.remove(X_CLASS);
     cell.classList.remove(CIRCLE_CLASS);
     cell.removeEventListener('click', handleClick);
-    cell.addEventListener('click', handleClick, { once: true})
-  })
-    setBoardHoverClass();
-    winningMsgElement.classList.remove('show');
-  };
-  resetBtn.addEventListener('click', startGame);
-  startGame();
-  document.getElementById("game-message").innerText = players[0] + "'s Turn"
-});
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMsgElement.classList.remove('show');
+  if (players[0] !== 'X') {
+    document.getElementById('game-message').innerText = `${players[0]}'s Turn`;
+  }
+};
+resetBtn.addEventListener('click', startGame);
+startGame();
+
 
 window.onload = function ready() {
   const playerForm = document.getElementById('player-form');
@@ -106,13 +97,12 @@ window.onload = function ready() {
     const ipt = item.getElementsByTagName('input');
     ipt[0].addEventListener('input', () => {
       if (playerForm.name1.value > '' && playerForm.name2.value > '') {
-        players[0]=playerForm.name1.value;
-        players[1]=playerForm.name2.value;
+        players[0] = playerForm.name1.value;
+        players[1] = playerForm.name2.value;
         btn.disabled = false;
       } else {
         btn.disabled = true;
-      }       
+      }
     });
   });
 };
-
