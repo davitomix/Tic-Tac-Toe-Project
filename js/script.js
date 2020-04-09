09592d8bfe9b12bb6cc5c3ce36d40b3c119a43f7
Feber <cochabambinoski@gmail.com>
2020-04-09 11:25:27 -0400

    fixing variables to work with the new logic

--- js/script.js
+++ js/script.js
@@ -10,14 +10,17 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-const players = ['X', 'O'];
-let whoseturn = 0;
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMsgElement = document.getElementById('winningMsg');
 const winningMsgTxtElement = document.querySelector('[data-winning-msg-txt]');
 const resetBtn = document.getElementById('resetBtn');
-
+// Staring game form. 
+const playerForm = document.getElementById('player-form');
+const btn = document.getElementById('submit');
+const select = document.querySelectorAll('.player-input');
+// Current player box.
+const currentPlayerBox = document.getElementById('current-player');
 
 window.onload = function ready() {
   board.classList.add('d-none');
