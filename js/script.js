354dcc24cc7f22a90dd05cbb1d5fedaeece67182
Feber <cochabambinoski@gmail.com>
2020-04-09 12:32:51 -0400

    Lintern issues fixed

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ const WINNING_COMBINATIONS = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
+  [2, 4, 6],
 ];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMsgElement = document.getElementById('winningMsg');
 const winningMsgTxtElement = document.querySelector('[data-winning-msg-txt]');
 const resetBtn = document.getElementById('resetBtn');
-// Staring game form. 
+// Staring game form.
 const playerForm = document.getElementById('player-form');
 const btn = document.getElementById('submit');
 const select = document.querySelectorAll('.player-input');
@@ -44,107 +44,96 @@ const Game = () => {
   const setBoardHoverClass = (turn) => {
     board.classList.remove(X_CLASS);
     board.classList.remove(CIRCLE_CLASS);
-    if(turn) {
+    if (turn) {
       board.classList.add(X_CLASS);
-    }
-    else {
+    } else {
       board.classList.add(CIRCLE_CLASS);
     }
   };
-  
+
   const placeMark = (cell, currentClass) => {
     cell.classList.add(currentClass);
   };
-
-  const checkWin = (currentClass) => {
-    return WINNING_COMBINATIONS.some(combination => {
-      return combination.every(index => {
-        return cellElements[index].classList.contains(currentClass);
-      })
-    })
-  };
-
+  /* eslint-disable */
+  const checkWin = (currentClass) => WINNING_COMBINATIONS.some(combination => combination.every(index => cellElements[index].classList.contains(currentClass)));
+  /* eslint-enable */
   const endGame = (draw, turn) => {
-      if(draw) {
-        winningMsgTxtElement.innerText = 'Draw!';
-      } else {
-        winningMsgTxtElement.innerText = `${turn ? "X's" : "O's"} Wins!`;
-      }
-      winningMsgElement.classList.add('show');
+    if (draw) {
+      winningMsgTxtElement.innerText = 'Draw!';
+    } else {
+      winningMsgTxtElement.innerText = `${turn ? "X's" : "O's"} Wins!`;
+    }
+    winningMsgElement.classList.add('show');
   };
-
-  const isDraw = () => {
-    return [...cellElements].every(cell => {
-      return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
-    })
-  };
-
+  /* eslint-disable */
+  const isDraw = () => [...cellElements].every(cell => cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS));
+  /* eslint-enaable */
   return {
     setBoardHoverClass,
     placeMark,
     checkWin,
     endGame,
-    isDraw
+    isDraw,
   };
 };
 
 const playersObj = (player1, player2) => {
-  playerX = player1;
-  playerO = player2;
-  return { playerX, playerO }
-};
-
-const initGame = (form) => {
-  const player1 = form[0].value;
-  const player2 = form[1].value;
-  const players = playersObj(player1, player2);
-  const playerX = players.playerX;
-  const playerO = players.playerO;
-  startGame(playerX, playerO);
+  const playerX = player1;
+  const playerO = player2;
+  return { playerX, playerO };
 };
 
 const startGame = (playerX, playerO) => {
+  const game = Game();
+  let turn;
+  let playX;
+  let playO;
   const handleClick = (e) => {
     const cell = e.target;
     const currentClass = turn ? X_CLASS : CIRCLE_CLASS;
     game.placeMark(cell, currentClass);
     // -----------
-    if(game.checkWin(currentClass)) {
-      console.log('Win!');
+    if (game.checkWin(currentClass)) {
       game.endGame(false, turn);
-    } else if(game.isDraw()) {
-       game.endGame(true, turn);
-     } else {
+    } else if (game.isDraw()) {
+      game.endGame(true, turn);
+    } else {
       turn = !turn;
       game.setBoardHoverClass(turn);
-      if(turn){
+      if (turn) {
         currentPlayerBox.innerHTML = `<h2> Its player ${playX} turn </h2>`;
-      }
-      else{
+      } else {
         currentPlayerBox.innerHTML = `<h2> Its player ${playO} turn </h2>`;
       }
     }
     // ----------
   };
-  const playX = playerX;
-  const playO = playerO;
+  playX = playerX;
+  playO = playerO;
   playerForm.classList.add('d-none');
   board.classList.remove('d-none');
   currentPlayerBox.classList.remove('d-none');
   currentPlayerBox.innerHTML = `<h2> Its player ${playX} turn </h2>`;
-  const game = Game();
-  let turn = X_TURN;
+  turn = X_TURN;
   game.setBoardHoverClass(turn);
-  resetBtn.addEventListener('click', function(){
+  resetBtn.addEventListener('click', () => {
     startGame(playerX, playerO);
   }, false);
   cellElements.forEach(cell => {
     cell.classList.remove(X_CLASS);
     cell.classList.remove(CIRCLE_CLASS);
     cell.removeEventListener('click', handleClick);
-    cell.addEventListener('click', handleClick, { once: true })
-  })
+    cell.addEventListener('click', handleClick, { once: true });
+  });
   winningMsgElement.classList.remove('show');
 };
-
-
+/* eslint-disable */
+const initGame = (form) => {
+  const player1 = form[0].value;
+  const player2 = form[1].value;
+  const players = playersObj(player1, player2);
+  const { playerX } = players;
+  const { playerO } = players;
+  startGame(playerX, playerO);
+};
+/* eslint-enable */
