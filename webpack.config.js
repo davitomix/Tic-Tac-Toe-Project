9ad300c12c6bc4bf696bccc51be6f333b41ca924
David Elì <davidelimg1@gmail.com>
2020-07-03 17:33:26 -0500

    Implementing webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable global-require */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+};
+/* eslint-enable global-require */
